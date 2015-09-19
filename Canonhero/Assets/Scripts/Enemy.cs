a2aa7a2fe5135a57d0df7a8e12360111f3ae91d1
Reynold Erwandi <reynolderwandi@gmail.com>
2015-09-19 18:07:35 +0700

    Added dragon (Prefabs, animation, dead)

--- Canonhero/Assets/Scripts/Enemy.cs
+++ Canonhero/Assets/Scripts/Enemy.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
-	// Use this for initialization
+    private Animator _Anim;
+    private Rigidbody2D _Rb2d;
+    private bool _IsDead = false;
+
 	void Start () {
-	
+        _Anim = GetComponent<Animator>();
+        _Rb2d = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(!_IsDead)
+        {
+            _Anim.SetTrigger("Dead");
+            Dead();
+        }    }
+
+    void Dead()
+    {
+        _IsDead = true;
+        _Rb2d.AddForceAtPosition(Vector2.up *  200.0f, transform.position);
+        _Rb2d.gravityScale = 1.0f;
+    }
 }
---
+++ Canonhero/Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSpawner : MonoBehaviour {
+
+    public GameObject Object;
+    public Transform SpawnPoint;
+    public float Interval;
+    public Vector2 OffsetMin;
+    public Vector2 OffsetMax;
+
+	void Start () {
+        InvokeRepeating("Spawn", 1.0f, Interval);
+	}
+	
+	void Update () {
+	
+	}
+
+    void Spawn()
+    {
+        float xPos = Random.Range(OffsetMin.x, OffsetMax.x);
+        float yPos = Random.Range(OffsetMin.y, OffsetMax.y);
+        Vector3 nextSpawn = SpawnPoint.position + new Vector3(xPos, yPos);
+        Instantiate(Object, nextSpawn, SpawnPoint.rotation);
+    }
+}
