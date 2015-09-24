59374a662678105c3f555060fd1fe1ba75d03315
Reynold Erwandi <reynolderwandi@gmail.com>
2015-09-24 10:48:54 +0700

    Add Base Character System

---
+++ Canonhero/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public Animator Anim;
+    public PlayerMovement Movement;
+    public RandomSpawner Spawner;
+    public GameObject MainMenuPanel;
+
+	void Start () {
+        Anim.enabled = false;
+        Movement.enabled = false;
+        Spawner.enabled = false;
+	}
+	
+	void Update () {
+	
+	}
+
+    public void StartGame()
+    {
+        MainMenuPanel.SetActive(false);
+        Anim.enabled = true;
+        Movement.enabled = true;
+        Spawner.enabled = true;
+    }
+}
